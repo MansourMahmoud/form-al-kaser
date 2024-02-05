2f0df0ba8addcf1436977f7ffe9ffb913fd1654e
MansourMahmoud <mansourmahmoud77a@gmail.com>
2024-02-05 18:58:48 +0200

    improve

--- src/pages/packages-deals/index.jsx
+++ src/pages/packages-deals/index.jsx
@@ -4,8 +4,8 @@ const PackagesDeals = ({ isDarkModeActive }) => {
   return (
     <div>
       {/* header */}
-      <div className="flex justify-center items-center border-b-[2px] border-darkMode-dark500 shadow-2xl p-5">
-        <p className="text-4xl">عروض الباقات</p>
+      <div className="flex justify-center items-center border-b-[2px] border-darkMode-dark500 shadow-2xl dark:shadow-xl dark:shadow-darkMode-dark500 p-5">
+        <p className="text-4xl dark:text-darkMode-dark50">عروض الباقات</p>
       </div>
       {/* end */}
     </div>
